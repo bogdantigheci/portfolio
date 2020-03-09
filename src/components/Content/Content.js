e09e69f0d6b726baf67fe72e8b6bdbf6a694dc74
Your NameBogdan <bogdantigheci@yahoo.com>
2020-03-09 13:08:29 +0200

    minor changes

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Content.css';
 const home = () => {
   return (
-    <div>
+    <div className="container">
       <div className="about container-fluid justify-content-center">
         <h1>About Me</h1>
         <p>
