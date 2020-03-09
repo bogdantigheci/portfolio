c219da3ae31634bbbad43994f7a88ae1dfc6e335
Your NameBogdan <bogdantigheci@yahoo.com>
2020-03-09 11:38:43 +0200

    added about section and css changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Content from "./components/Content/Content";
-import Footer from "./components/Footer/Footer";
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,35 +2,49 @@ import React from 'react';
 import './Content.css';
 const home = () => {
   return (
-    <div className="container">
-      <a
-        href="https://burgerapp-3d813.web.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="img/img1.png" alt="project" />
-      </a>
-      <a
-        href="https://todoreactbt.netlify.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="img/img2.png" alt="project" />
-      </a>
-      <a
-        href="https://phonestorebt.netlify.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="img/img3.png" alt="project" />
-      </a>
-      <a
-        href="https://tictactoebt.netlify.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="img/img4.png" alt="project" />
-      </a>
+    <div>
+      <div className="about container-fluid">
+        <h1>About Me</h1>
+        <p>
+          Looking for a job as <b>Frontend Developer</b>.
+        </p>
+        <div> Computer Science graduate. In my final masters year.</div>
+        <div>
+          Eager to learn and be part of a professional environment. Currently
+          learning and <b>React+Redux</b>.
+        </div>
+        <div>You can check out some of my projects below.</div>
+      </div>
+      <div className="container d-flex justify-content-center">
+        <a
+          href="https://burgerapp-3d813.web.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="img/img1.png" alt="project" />
+        </a>
+        <a
+          href="https://todoreactbt.netlify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="img/img2.png" alt="project" />
+        </a>
+        <a
+          href="https://phonestorebt.netlify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="img/img3.png" alt="project" />
+        </a>
+        <a
+          href="https://tictactoebt.netlify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="img/img4.png" alt="project" />
+        </a>
+      </div>
     </div>
   );
 };
