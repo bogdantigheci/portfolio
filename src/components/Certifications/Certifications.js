1ee91804063b3973e983130cd53711717c937079
Your NameBogdan <bogdantigheci@yahoo.com>
2020-04-08 11:29:53 +0300

    fixed img gallery pos

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -5,15 +5,19 @@ import './Certifications.css';
 const images = [
   {
     original: 'img/css3_in_depth.jpg',
+    thumbnail: 'img/css3_in_depth.jpg',
   },
   {
     original: 'img/introduction_to_sql.jpg',
+    thumbnail: 'img/introduction_to_sql.jpg',
   },
   {
     original: 'img/java_fundamentals_the_java_language.jpg',
+    thumbnail: 'img/java_fundamentals_the_java_language.jpg',
   },
   {
     original: 'img/java_fundamentals_collections.jpg',
+    thumbnail: 'img/java_fundamentals_collections.jpg',
   },
 ];
 const additionalClass = ['img_gal_cert'];
