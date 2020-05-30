a1b88b5c7be218dfa8a3571d7c5545417d88ea07
Your NameBogdan <bogdantigheci@yahoo.com>
2020-05-30 16:19:53 +0300

    added port image meta

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,13 +8,22 @@ class Content extends Component {
       <div className="container cnt">
         <div className="about container-fluid justify-content-center">
           <h1>About Me</h1>
-          <p>
-            Looking for a job as <b>Frontend Developer</b>.
-          </p>
-          <div> Computer Science graduate. In my final masters year.</div>
-          <div>
-            Eager to learn and be part of a professional environment. Currently
-            learning and <b>React+Redux</b>.
+          <div className="about_section">
+            Computer Science graduate, Bachelor’s and Master’s degrees. Seeking
+            a new job opportunity as a Frontend Developer. Eager to learn and
+            acquire on-the-job experience and be part of a professional team.
+          </div>
+          <div className="about_section">
+            I like attending online meetings from the JavaScript community and
+            watching online courses regarding technologies I’m interested in. I
+            also enjoy taking on challenging projects and seeing them through.
+          </div>
+          <div className="about_section">
+            I’ve developed my dissertation project using ReactJS, Redux,
+            JavaScript (ES6+), HTML, CSS, NodeJS, Express, REST API, MongoDB,
+            Git, Npm and Postman. I also have basic knowledge of CSS
+            Preprocessors (SASS, LESS), Bootstrap, Java, Sql, MySql, PHP and
+            C++.
           </div>
           <div>You can check out some of my projects below.</div>
         </div>
